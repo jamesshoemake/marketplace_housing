0622f494a722b2358cf9851430833b020d1ef8aa
James Shoemake <james.shoemake1@gmail.com>
2022-09-20 11:44:10 -0500

    Added user sign up, sign in, and google auth.

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,26 @@
+import { useEffect, useState, useRef } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+export const useAuthStatus = () => {
+  const isMounted = useRef(true)
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [checkingStatus, setCheckingStatus] = useState(true)
+  const auth = getAuth()
+
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true)
+        }
+
+        setCheckingStatus(false)
+      })
+    }
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
+
+  return { loggedIn, checkingStatus }
+}
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,98 @@
-import React from 'react'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { ReactComponent as ArrowRighticon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import OAuth from '../components/OAuth'
 
 const SignIn = () => {
-  return <div>Sign In</div>
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value
+    }))
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+
+      if (userCredential.user) {
+        navigate('/')
+      }
+    } catch (error) {
+      toast.error('Bad user Credentials')
+    }
+  }
+
+  return (
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back</p>
+        </header>
+        <main>
+          <form onSubmit={onSubmit}>
+            <input
+              type='email'
+              className='emailInput'
+              placeholder='Email'
+              id='email'
+              value={email}
+              onChange={onChange}
+            />
+            <div className='passwordInputDiv'>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className='passwordInput'
+                placeholder='Password'
+                id='password'
+                value={password}
+                onChange={onChange}
+              />
+              <img
+                src={visibilityIcon}
+                alt='show password'
+                className='showPassword'
+                onClick={() => setShowPassword((prev) => !prev)}
+              />
+            </div>
+            <Link to='/forgoet-password' className='forgotPasswordLink'>
+              Forgot Password
+            </Link>
+
+            <div className='signInBar'>
+              <p className='signInText'>Sign In</p>
+              <button className='signInButton'>
+                <ArrowRighticon fill='#ffffff' width='34px' height='34px' />
+              </button>
+            </div>
+          </form>
+          <OAuth />
+          <Link to='/sign-up' className='registerLink'>
+            Sign Up
+          </Link>
+        </main>
+      </div>
+    </>
+  )
 }
 
 export default SignIn
